68c275e62703d2986d1570c9138b276dddf2e1e4
StevenKrzysztof <48268079+StevenKrzysztof@users.noreply.github.com>
2023-05-04 10:56:37 -0400

    Update Maskshow.py
    newest

--- Maskshow.py
+++ Maskshow.py
@@ -4,10 +4,9 @@ from PyQt5.Qt import QPixmap, QPoint, Qt, QPainter
 import sys
 from PyQt5.QtGui import *
 
-
 class ImageBox(QWidget):
     def __init__(self):
-        super(ImageBox, self).__init__()
+        super().__init__()
         self.img = None
         self.scaled_img = None
         self.point = QPoint(0, 0)
@@ -16,114 +15,99 @@ class ImageBox(QWidget):
         self.left_click = False
         self.scale = 0.5
 
+        self.init_ui()
+
     def init_ui(self):
         self.setWindowTitle("ImageBox")
-#select the mask from the output path and show
-    def set_image(self, Maskpath):
-        
-        Maskpath = './images/result.jpg'
-        self.img = QPixmap(Maskpath)
-        
-        self.scaled_img = self.img.scaled(self.size())
-        
-        
 
-    
+    def set_image(self, mask_path):
+        self.img = QPixmap(mask_path)
+        self.scaled_img = self.img.scaled(self.size())
+
     def paintEvent(self, e):
-        """
-        receive paint events
-        :param e: QPaintEvent
-        :return:
-        """
         if self.scaled_img:
-            painter = QPainter()
-            painter.begin(self)
+            painter = QPainter(self)
             painter.scale(self.scale, self.scale)
             painter.drawPixmap(self.point, self.scaled_img)
-            painter.end()
- 
-    #action for mouse move
+
     def mouseMoveEvent(self, e):
-      
         if self.left_click:
-            self.end_pos = e.pos() - self.start_pos
-            self.point = self.point + self.end_pos
-            self.start_pos = e.pos()
+            if self.start_pos is None:
+                self.start_pos = e.pos()
+            else:
+                self.end_pos = e.pos() - self.start_pos
+                self.point += self.end_pos
+                self.start_pos = e.pos()
             self.repaint()
- #action for mouse press
+
     def mousePressEvent(self, e):
-      
         if e.button() == Qt.LeftButton:
             self.left_click = True
             self.start_pos = e.pos()
-  #action for mouse release
+
     def mouseReleaseEvent(self, e):
-        
         if e.button() == Qt.LeftButton:
             self.left_click = False
+            self.start_pos = None
+            self.end_pos = None
+
 
 #the app window for show picture and adjust
 class MainDemo(QWidget):
     def __init__(self):
-        super(MainDemo, self).__init__()
+        super().__init__()
 
         self.setWindowTitle("Result Show")
         self.setFixedSize(600, 600)
 
-        
-        
-        # zoom in and out button
+        # zoom in and out buttons
         self.zoom_in = QPushButton("+")
         self.zoom_in.clicked.connect(self.large_click)
         self.zoom_in.setFixedSize(30, 30)
-        
 
         self.zoom_out = QPushButton("-")
         self.zoom_out.clicked.connect(self.small_click)
         self.zoom_out.setFixedSize(30, 30)
-        
-        
 
-        w = QWidget(self)
-        layout = QHBoxLayout()
-        
-        layout.addWidget(self.zoom_in)
-        layout.addWidget(self.zoom_out)
-        layout.setAlignment(Qt.AlignLeft)
-        w.setLayout(layout)
-        w.setFixedSize(550, 50)
+        # layout for buttons
+        buttons_layout = QHBoxLayout()
+        buttons_layout.addWidget(self.zoom_in)
+        buttons_layout.addWidget(self.zoom_out)
+        buttons_layout.setAlignment(Qt.AlignLeft)
+
+        # widget for buttons layout
+        buttons_widget = QWidget(self)
+        buttons_widget.setLayout(buttons_layout)
+        buttons_widget.setFixedSize(550, 50)
 
         self.box = ImageBox()
         self.box.resize(512, 512)
-        
-        Maskname = ('./result.jpg')
-        self.box.set_image(Maskname)
+
+        mask_path = './images/result.jpg'
+        self.box.set_image(mask_path)
         self.update()
 
-        layout = QVBoxLayout()
-        layout.addWidget(w)
-        layout.addWidget(self.box)
-        self.setLayout(layout)
+        # layout for entire widget
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(buttons_widget)
+        main_layout.addWidget(self.box)
+        self.setLayout(main_layout)
 
-    #open image from result path
+    # open image from result path
     def open_image(self):
-        
-        
-        Maskname = ('./result.jpg')
-        self.box.set_image(Maskname)
+        mask_path = './images/result.jpg'
+        self.box.set_image(mask_path)
         self.update()
-        
-    
-    #zoom in function
+
+    # zoom in function
     def large_click(self):
-        
         if self.box.scale < 2:
             self.box.scale += 0.1
             self.box.adjustSize()
             self.update()
-    #zoom out function
+
+    # zoom out function
     def small_click(self):
-        
         if self.box.scale > 0.1:
             self.box.scale -= 0.2
             self.box.adjustSize()
